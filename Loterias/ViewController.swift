123c9bf10def445decca3bf64de90f6d23ed8015
José Scanavini <development@izagro.com.br>
2018-10-19 08:37:24 -0300

    Initial Commit

---
+++ Loterias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loterias
+//
+//  Created by José Guilherme Scanavini on 19/10/18.
+//  Copyright © 2018 teste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

0f0794475716db5eedad0d556ebce44ed5bb8625
José Guilherme Scanavini <development@izagro.com.br>
2018-10-19 10:35:55 -0300

    funcionalidades OK

--- Loterias/ViewController.swift
+++ Loterias/ViewController.swift
@@ -13,7 +13,20 @@ enum GameType : String{
     case quina = "Quina"
 }
 
+infix operator >--<
 
+func >--< (total: Int,universe:Int)->[Int]{
+    var result: [Int] = []
+    while result.count < total{
+        let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        
+        if !result.contains(randomNumber){
+            result.append(randomNumber)
+        }
+    }
+    
+    return result.sorted()
+}
 
 
 class ViewController: UIViewController {
@@ -24,15 +37,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        showNumber(for: .megasena)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showNumber(for type:GameType){
+        lbgameType.text = type.rawValue
+        var game: [Int] = []
+        
+        switch type{
+        case .megasena:
+            game = 6>--<60
+            balls.last!.isHidden = false
+        case .quina:
+            game = 6>--<60
+            balls.last!.isHidden = true
+        }
+        for (index,game) in game.enumerated(){
+            balls[index].setTitle("\(game)", for: .normal)
+        }
+        
+    }
 
 
     @IBAction func generatedGame() {
+        switch scGameType.selectedSegmentIndex {
+        case 0:
+            showNumber(for: .megasena)
+        default:
+            showNumber(for: .quina)
+        }
     }
 }
 
